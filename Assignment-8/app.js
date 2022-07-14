988d369fc89592b351c4a2de36fe26e0c0e8e707
Muhammad Shahrukh <104302450+Shahrukh02@users.noreply.github.com>
2022-07-14 15:52:13 +0500

    Add files via upload

---
+++ Assignment-8/app.js
@@ -0,0 +1,79 @@
+//  ================== TASK # 1 ================== 
+
+
+// var userInput =  prompt("Enter an integer");
+// var num = Number(userInput)
+// var round_of = Math.round(userInput);
+// var floor = Math.floor(userInput);
+// var ceil = Math.ceil(userInput);
+// document.write("Number :  " + num + "<br>" +"Round Of : " + round_of + "<br>" + "floor value : " + floor + "<br>" + "Ceil value : " +  ceil)
+
+
+//  ================== TASK # 2 ================== 
+
+
+// var negPoint = prompt("Enter Any negative floating point");
+// var conCheck = negPoint.charAt(0);
+// console.log(conCheck)
+// if(conCheck === "-"){
+//     var num = Number(negPoint)
+//     var round_of = Math.round(negPoint);
+//     var floor = Math.floor(negPoint);
+//     var ceil = Math.ceil(negPoint);
+//     document.write("Number :  " + num + "<br>" +"Round Of : " + round_of + "<br>" + "floor value : " + floor + "<br>" + "Ceil value : " +  ceil)
+// }else{
+//     alert("Your Input isn't a negative Integer")
+// } 
+
+
+//  ================== TASK # 3 ================== 
+
+
+// var num = prompt("Enter Any Number");
+// var checkAbs = Math.abs(num);
+// document.write("The Absolute Number of " + num + " is " + checkAbs)
+
+
+//  ================== TASK # 4 ================== 
+
+// var random = Math.floor(Math.random() * 6 +1 );
+// document.write("Random Value is  " + "random)
+
+
+//  ================== TASK # 5 ================== 
+
+// var ran = Math.floor(Math.random() * 2);
+// if(ran == "1"){
+//     document.write("Random coin value : HEAD")
+// }else if(ran == "0"){
+//     document.write("Random coin value : TAILS")
+// }
+
+
+//  ================== TASK # 6 =================
+
+// var ranNum = Math.floor(Math.random() * 99 + 1)
+// document.write("Random Number Between 1 And 100 is : " + ranNum)
+
+//  ================== TASK # 7 ================== 
+
+// var userInput = prompt("Enter your weight in kilogram");
+
+//     if(userInput.indexOf('.')){
+        
+//         document.write("The Weight of user is : " + parseFloat(userInput) + "Kilogram")
+//     }
+//     else{
+//         document.write("The Weight of user is : " + parseInt(userInput) + "Kilogram")    
+//     }
+
+
+//  ================== TASK # 8 ================== 
+
+// var keeper = Math.floor(Math.random() * 9 + 1);
+// var takeNum = +prompt("Enter Any Number");
+// if(takeNum === keeper){
+//     alert("Congrtulation's You Guess The Right NUmber :)")
+// }else{
+//     alert("Try Again The Secret Number was  " + keeper )
+// }
